e77ac86eeb0a973ed6589a72a116be45a3084f40
sjhong310 <sjhong310@contec.kr>
2021-07-27 16:41:00 +0900

    Remove meaningless condition

--- mmcv/runner/checkpoint.py
+++ mmcv/runner/checkpoint.py
@@ -277,16 +277,9 @@ def load_from_http(filename, map_location=None, model_dir=None):
     Returns:
         dict or OrderedDict: The loaded checkpoint.
     """
-    rank, world_size = get_dist_info()
-    rank = int(os.environ.get('LOCAL_RANK', rank))
-    if rank == 0:
-        checkpoint = model_zoo.load_url(
-            filename, model_dir=model_dir, map_location=map_location)
-    if world_size > 1:
-        torch.distributed.barrier()
-        if rank > 0:
-            checkpoint = model_zoo.load_url(
-                filename, model_dir=model_dir, map_location=map_location)
+    checkpoint = model_zoo.load_url(
+        filename, model_dir=model_dir, map_location=map_location)
+
     return checkpoint
 
 
